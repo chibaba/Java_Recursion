b2aa3c97b6673787434a9885780aa227cc13ea91
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-26 15:54:47 +0100

    Using recurison to overflow a stack

---
+++ src/main/java/RunForever.java
@@ -0,0 +1,10 @@
+public class RunForever {
+    public int add(int num) {
+        return add(num + 1);
+    }
+    public static void main(String[] args) {
+        RunForever runForever = new RunForever();
+        System.out.println(runForever.add(1));
+
+    }
+}

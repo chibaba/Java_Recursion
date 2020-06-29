f79600d3fa8c63226511434044d816949d082ac7
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-06-29 08:30:34 +0100

    transversing xml

--- src/main/java/XmlTraverser.java
+++ src/main/java/XmlTraverser.java
@@ -1,7 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
 
 public class XmlTraverser {
     public Document loadXml() {
@@ -10,6 +15,24 @@ public class XmlTraverser {
             DocumentBuilder builder = factory.newDocumentBuilder();
             File xmlFile = new File("src/main/resources/cities.xml");
             document = builder.parse(xmlFile);
-        } catch (SAXException e)
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        return document;
     }
+
+    public void printText(String text) {
+        if (StringUtils.isNotBlank(text)) {
+            System.out.print(text);
+        }
+    }
+
+    public void indent(int indentation) {
+        System.out.print(StringUtils.leftPad("", indentation));
+    }
+
 }
